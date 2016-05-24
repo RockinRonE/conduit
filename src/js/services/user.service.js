1955463d04e9786a92d67f681910aeb0cc9db76e
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-05-24 16:17:07 -0500

    wired up to the server

--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -1,5 +1,33 @@
 export default class User {
-	constructor() {
+	constructor(AppConstants, $http) {
 		'ngInject'; 
+
+		this._AppConstants = AppConstants; // underscore indicates a service
+		this._$http = $http; 
+
+		//Object to store our user properties
+		this.current = null; 
 	}
+
+	//Try to authenticate by registering or loggin in
+
+	attemptAuth(type, credentials) {
+		let route = (type === 'login') ? '/login' : '';
+		return this._$http({
+			url: this._AppConstants.api + '/users' + route,
+			method: 'POST',
+			data: {
+				user: credentials
+			}
+		}).then(
+		//on success
+		(res) => {
+			// Store the user's info for easy lookup
+			this.current = res.data.user;
+
+			return res; 
+		}
+		);
+	}
+
 }
