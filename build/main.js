db36470a283747af068349a7b274ad6f478db269
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-06-20 19:26:36 -0500

    finalized updating

--- build/main.js
+++ build/main.js
@@ -36398,11 +36398,23 @@ var Articles = function () {
 	_createClass(Articles, [{
 		key: 'save',
 		value: function save(article) {
-			var request = {
-				url: this._AppConstants.api + '/articles',
-				method: 'POST',
-				data: { article: article }
-			};
+			var request = {};
+
+			// If there's a slug, perform an update via PUT w/ article's slug
+			if (article.slug) {
+				request.url = this._AppConstants.api + '/articles/' + article.slug;
+				request.method = 'PUT';
+				// Delete the slug from the article to ensure the server updates the slug
+				// which happens if the title of the article changed
+				delete article.slug;
+
+				// Otherwise, this is a new article POST request
+			} else {
+					request.url = this._AppConstants.api + '/articles';
+					request.method = 'POST';
+				}
+
+			request.data = { article: article };
 
 			return this._$http(request).then(function (res) {
 				return res.data.article;
--- src/js/services/articles.service.js
+++ src/js/services/articles.service.js
@@ -10,11 +10,25 @@ export default class Articles {
 	// Create an article
 
 	save(article) {
-		let request = {
-			url: `${this._AppConstants.api}/articles`,
-			method: 'POST',
-			data: { article: article }
-		};
+		let request = {};
+
+		// If there's a slug, perform an update via PUT w/ article's slug
+		if (article.slug) {
+			request.url = `${this._AppConstants.api}/articles/${article.slug}`;
+			request.method = 'PUT';
+			// Delete the slug from the article to ensure the server updates the slug
+			// which happens if the title of the article changed
+			delete article.slug; 
+
+			// Otherwise, this is a new article POST request
+		} else {
+			request.url = `${this._AppConstants.api}/articles`;
+			request.method = 'POST'; 
+		}
+
+
+		
+		request.data = { article: article };
 
 		return this._$http(request).then((res) => res.data.article);
 	}
@@ -26,5 +40,5 @@ export default class Articles {
 			method: 'GET'
 		}).then((res) => res.data.article);
 	}
-	
+
 }
